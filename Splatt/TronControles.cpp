9b72751e4fc14c19ccd5cb0da94a065937776a8c
Dev1ous <glanton.luke97@gmail.com>
2020-11-01 02:02:30 +0100

    Rajout possibilité IA intelligente prend le controle dès qu'on va se suicide

--- Splatt/TronControles.cpp
+++ Splatt/TronControles.cpp
@@ -9,64 +9,97 @@ extern bool isJ1_ia;
 void TronControlesClavierJ1()
 {
 	if (isButtonPressed(Action::TronJ1_Gauche))
+	{
 		if (joueur1.getDir() != 2)
 		{
 			joueur1.setDir(1);
 			joueur1.getSprite().setRotation(180);
 			isJ1_ia = false;
 		}
+	}
+	/*else
+		isJ1_ia = true;*/
 	if (isButtonPressed(Action::TronJ1_Droite))
+	{
 		if (joueur1.getDir() != 1)
 		{
 			joueur1.setDir(2);
 			joueur1.getSprite().setRotation(0);
 			isJ1_ia = false;
 		}
+	}
+	/*else
+		isJ1_ia = true;*/
+			
 	if (isButtonPressed(Action::TronJ1_Haut))
+	{
 		if (joueur1.getDir() != 0)
 		{
 			joueur1.setDir(3);
 			joueur1.getSprite().setRotation(270);
 			isJ1_ia = false;
 		}
+	}
+	/*else 
+		isJ1_ia = true;*/
 	if (isButtonPressed(Action::TronJ1_Bas))
+	{
 		if (joueur1.getDir() != 3)
 		{
 			joueur1.setDir(0);
 			joueur1.getSprite().setRotation(90);
 			isJ1_ia = false;
 		}
+	}
+	/*else
+		isJ1_ia = true;*/
 }
 void TronControlesClavierJ2()
 {
 	if (isButtonPressed(Action::TronJ2_Gauche))
+	{
 		if (joueur2.getDir() != 2)
 		{
 			joueur2.setDir(1);
 			joueur2.getSprite().setRotation(180);
 			isJ2_ia = false;
 		}
+	}
+	/*else
+		isJ2_ia = true;*/
 	if (isButtonPressed(Action::TronJ2_Droite))
+	{
 		if (joueur2.getDir() != 1)
 		{
 			joueur2.setDir(2);
 			joueur2.getSprite().setRotation(0);
 			isJ2_ia = false;
 		}
+	}
+	/*else
+		isJ2_ia = true;*/
 	if (isButtonPressed(Action::TronJ2_Haut))
+	{
 		if (joueur2.getDir() != 0)
 		{
 			joueur2.setDir(3);
 			joueur2.getSprite().setRotation(270);
 			isJ2_ia = false;
 		}
+	}
+	/*else
+		isJ2_ia = true;*/
 	if (isButtonPressed(Action::TronJ2_Bas))
+	{
 		if (joueur2.getDir() != 3)
 		{
 			joueur2.setDir(0);
 			joueur2.getSprite().setRotation(90);
 			isJ2_ia = false;
 		}
+	}
+	/*else
+		isJ2_ia = true;*/
 }
 void GoMenuIG()
 {
